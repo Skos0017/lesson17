715480fcd29d84b71b35b90e94866a94a4d0efd6
Костя <96836013+Skos0017@users.noreply.github.com>
2025-02-26 20:34:33 +0200

    18 пара

--- src/App.js
+++ src/App.js
@@ -1,55 +1,39 @@
 import { useEffect, useState } from "react";
 
-  // function useTimer(interval){
-  //   const [seconds, setSeconds] = useState(0) ;
-
-  //   useEffect(
-  //     () => {
-  //       const timer = setInterval(() => {
-  //         setSeconds((prevValue) => prevValue +1)
-  //       }, interval);
-  //       return () => clearInterval(timer);
-  //     },
-  //     [interval]
-  //   )
-  //   return seconds;
-  // }
-
-  // function TimerComponent(){
-  //   const seconds = useTimer(1000);
-  //   return <div>Прошло секунд: {seconds}</div>
-  // }
-
-  function useEffect(url){
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, seterror] = useState(null);
-
-    useEffect(() => {
-      const fetchData = async () => {
-        try{
-          const response = await fetch(url);
-          const result = await response.json();
-          setData(result);
-        }catch(error){
-          setError(error);
-        }finally{
-          setLoading(false);
-        }
-      }
-    },[url])
-    return {data, loading, error}
-  }
-
-  function DataComponent(){
-    const {data, loading, error} = useEffect('https://jsonplaseholder.typicode.com/posts');
-  }
-
+function TimerComponent(){
+  
+}
+  
 function App() {
   return (
-    // <TimerComponent/>
-    
+  
   )
 }
 
 export default App;
+
+/*
+Задача. Хук для отслеживания активности пользователя.
+Необходимо реализовать пользовательский хук, который будет отслеживать активность
+пользователя, такую как клики и движение мыши. Если пользователь не выполняет
+никаких действий в течение 30 секунд, его состояние меняется на "неактивен". Хук
+должен возвращать два значения: флаг активности (isActive) и время последнего
+взаимодействия пользователя с приложением.
+Шаги для реализации:
+Создание основного состояния активности
+Нам нужно отслеживать, активен ли пользователь. Для этого создадим состояние
+isActive, которое будет по умолчанию true, и будет изменяться на false, если пользователь
+неактивен в течение 30 секунд.
+Отслеживание событий активности
+Нам нужно отслеживать действия пользователя, такие как клики и движение мыши. Для
+этого будем использовать addEventListener для событий mousemove и click, чтобы знать,
+когда пользователь был активен в последний раз.
+Таймер для проверки активности
+Используем таймер, который будет срабатывать каждые 30 секунд. Если за это время не
+произошло никаких событий активности, флаг активности меняется на false.
+Сброс таймера при активности
+При каждом взаимодействии с пользователем (клик, движение мыши) нужно сбрасывать
+таймер и устанавливать новое время последнего взаимодействия.
+Возврат состояния
+Хук должен возвращать флаг активности и время последнего взаимодействия
+*/
