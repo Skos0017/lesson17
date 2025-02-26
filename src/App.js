9773cc8fd357a6de48d9a86b6ec593c3e72680f9
Костя <96836013+Skos0017@users.noreply.github.com>
2025-02-26 19:35:35 +0200

    123

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
 import { useEffect, useState } from "react";
 
-  function useTimer(interval){
-    const [seconds, setSeconds] = useState(0) ;
+  // function useTimer(interval){
+  //   const [seconds, setSeconds] = useState(0) ;
 
-    useEffect(
-      () => {
-        const timer = setInterval(() => {
-          setSeconds((prevValue) => prevValue +1)
-        }, interval);
-        return () => clearInterval(timer);
-      },
-      [interval]
-    )
-    return seconds;
+  //   useEffect(
+  //     () => {
+  //       const timer = setInterval(() => {
+  //         setSeconds((prevValue) => prevValue +1)
+  //       }, interval);
+  //       return () => clearInterval(timer);
+  //     },
+  //     [interval]
+  //   )
+  //   return seconds;
+  // }
+
+  // function TimerComponent(){
+  //   const seconds = useTimer(1000);
+  //   return <div>Прошло секунд: {seconds}</div>
+  // }
+
+  function useEffect(url){
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, seterror] = useState(null);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try{
+          const response = await fetch(url);
+          const result = await response.json();
+          setData(result);
+        }catch(error){
+          setError(error);
+        }finally{
+          setLoading(false);
+        }
+      }
+    },[url])
+    return {data, loading, error}
   }
 
-  function TimerComponent(){
-    const seconds = useTimer(1000);
-    return <div>Прошло секунд: {seconds}</div>
+  function DataComponent(){
+    const {data, loading, error} = useEffect('https://jsonplaseholder.typicode.com/posts');
   }
 
 function App() {
   return (
-    <TimerComponent/>
+    // <TimerComponent/>
+    
   )
 }
 
